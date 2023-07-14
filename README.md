3c29c012e3f616f382c61bb14a80a7fed740804f
andreiilinca <valentin.andrei.i@outlook.com>
2023-07-14 17:01:32 +0300

    CV

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# andreiilinca.github.io
-my CV
+# Andrei Ilinca's CV
+
+My personal CV made with `HTML`, `CSS` and pure `JS`.
+
+## 🎞 Live Preview
+
+[andreiilinca.github.io](https://andreiilinca.github.io/)
+
+## 📋 Developer Notes
+
+To start web server use:
+
+- `CTRL + Shift + P`
+- **Browser Sync: Server mode in browser**
+- type `\` and press `Enter`
---
+++ examples.js
@@ -0,0 +1,39 @@
+var employed = false; // boolean
+var age = 24; // number
+var name = "First Last"; // string
+
+age = 30;
+
+// object (array)
+var skills = ["html", "css", "js"];
+// object (json)
+var person = {
+  employed: true,
+  age: 29,
+};
+
+// object (DOM, other)
+var el = document.getElementById("el");
+
+name = "Andrei";
+
+function printInfo() {
+  console.log(typeof skills, skills, age);
+  console.info("my name", name);
+  console.warn("person type", typeof person, person);
+}
+
+var fVariable = function () {
+  console.info("functie ca variabila");
+};
+
+var f1Variable = () => {
+  console.info("functie ca variabila");
+};
+
+printInfo();
+fVariable();
+
+console.info("t1", typeof printInfo);
+console.info("t2", typeof fVariable);
+console.info("t4", typeof f1Variable);
